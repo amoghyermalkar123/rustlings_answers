fd56ffd5c3c969fc391677e07a642189ef784adc
ayermalkar123 <ayermalkar99@gmail.com>
2024-05-16 23:27:27 +0530

    add: complete hashmap series

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,11 +14,10 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
+#[derive(Debug)]
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
@@ -34,36 +33,31 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         let team_1_score: u8 = v[2].parse().unwrap();
         let team_2_name = v[1].to_string();
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
 
         if let Some(t1) = scores.get(&team_1_name) {
             let team = Team {
                 goals_scored: team_1_score + t1.goals_scored,
-                goals_conceded: (team_1_score + team_2_score) - team_1_score,
+                goals_conceded: t1.goals_conceded + team_2_score,
             };
-            scores.insert(team_1_name, team);
+            scores.insert(team_1_name.clone(), team);
         } else {
             let team = Team {
                 goals_scored: team_1_score,
-                goals_conceded: (team_1_score + team_2_score) - team_1_score,
+                goals_conceded: team_2_score,
             };
-            scores.insert(team_1_name, team);
+            scores.insert(team_1_name.clone(), team);
         }
 
-        if let Some(t2) = scores.get(&team_1_name) {
+        if let Some(t2) = scores.get(&team_2_name) {
             let team2 = Team {
                 goals_scored: team_2_score + t2.goals_scored,
-                goals_conceded: (team_1_score + team_2_score) - team_2_score,
+                goals_conceded: t2.goals_conceded + team_1_score,
             };
             scores.insert(team_2_name, team2);
         } else {
             let team2 = Team {
                 goals_scored: team_2_score,
-                goals_conceded: (team_1_score + team_2_score) - team_2_score,
+                goals_conceded: team_1_score,
             };
             scores.insert(team_2_name, team2);
         }
